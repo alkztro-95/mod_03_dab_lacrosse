7bd527e16c8c9fd1b2f625e8bb548e07c4a674ca
Alfredo Castro <alkztro@gmail.com>
2026-08-24 19:42:25 -0600

    feat: Add deployment comment to bronze_sales_transactions.sql

--- src/bronze/bronze_sales_transactions.sql
+++ src/bronze/bronze_sales_transactions.sql
@@ -1,6 +1,7 @@
 -- Bronze Layer: Sales Transactions (Main Fact Table)
 -- Ingest sales transaction data with Auto Loader
 -- Links customers and stores through foreign keys
+-- Add a comment for deployment.
 
 CREATE OR REFRESH STREAMING TABLE ${catalog}.${schema_bronze}.bronze_sales_transactions
 COMMENT 'Bronze layer - Raw sales transaction data ingested with Auto Loader.'
